ea1194aa9bd40edfb5af41d4d5828577930cbb3e
MadhavNainala <nainala.p@northeastern.edu>
2024-02-03 23:36:09 -0500

    added resource definitions

---
+++ variables.tf
@@ -0,0 +1,9 @@
+# S3 bucket name
+variable "bucket-name" {
+  default = "aws_terraform"
+}
+
+# Domain name that you have registered
+variable "domain-name" {
+  default = "maddy.live" // Modify as per your domain name
+}
